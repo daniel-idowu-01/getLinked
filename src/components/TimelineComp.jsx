18044dea886b220c525124e3e75dbc7947ece181
daniel-idowu-01 <danielidowu414@gmail.com>
2023-10-06 17:43:20 +0100

    updated timeline

--- src/components/TimelineComp.jsx
+++ src/components/TimelineComp.jsx
@@ -1,118 +1,57 @@
 import React from 'react'
+import timelineData from '../data/Timeline.json'
 import 'react-vertical-timeline-component/style.min.css';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import { TbNumber1, TbNumber2, TbNumber3, TbNumber4, TbNumber5, TbNumber6 } from 'react-icons/tb'
 
 const TimelineComp = () => {
   return (
-    <section id='timeline'>
-      
-      <article className='flex justify-center items-center'>
-        <div className='md:w-1/2 text-center text-white p-20 pb-10'>
-          <p className='clash-display text-[20px] md:text-[32px] z-20'>
-            Timeline
-          </p>
-          <p className='montserrat leading-relaxed text-base z-20'>
-            Here is the breakdown of the time we anticipate using for the upcoming event
-          </p>
-        </div>
-      </article>
-      
-
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: '#D434FE', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid #D434FE' }}
-          date="November 18, 2023"
-          iconStyle={{ background: 'linear-gradient(270deg, #903AFF 0%, #FE34B9 100%)', color: '#fff' }}
-          icon={<TbNumber1 />}
-        >
-          <h3 className="vertical-timeline-element-title text-lg font-semibold">Hackathon Announcement</h3>
-          <p>
-          The getlinked tech hackathon 1.0 is formally announced to the general public and teams begin to get ready to register
-          </p>
-        </VerticalTimelineElement>
-
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: '#D434FE', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid #D434FE' }}
-          date="November 18, 2023"
-          iconStyle={{ background: 'linear-gradient(270deg, #903AFF 0%, #FE34B9 100%)', color: '#fff' }}
-          icon={<TbNumber2 />}
-        >
-          <h3 className="vertical-timeline-elemen text-lg font-semibold">Teams Registration begins</h3>
-          <p>
-          Interested teams can now show their interest in the getlinked tech hackathon 1.0 2023 by proceeding to register
-          </p>
-        </VerticalTimelineElement>
-
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: '#D434FE', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid #D434FE' }}
-          date="November 18, 2023"
-          iconStyle={{ background: 'linear-gradient(270deg, #903AFF 0%, #FE34B9 100%)', color: '#fff' }}
-          icon={<TbNumber3 />}
-        >
-          <h3 className="vertical-timeline-elemen text-lg font-semibold">Teams Registration ends</h3>
-          <p>
-            Interested Participants are no longer Allowed to register
-          </p>
-        </VerticalTimelineElement>
-
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: '#D434FE', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid #D434FE' }}
-          date="November 18, 2023"
-          iconStyle={{ background: 'linear-gradient(270deg, #903AFF 0%, #FE34B9 100%)', color: '#fff' }}
-          icon={<TbNumber4 />}
-        >
-          <h3 className="vertical-timeline-e text-lg font-semibold">Announcement of the accepted teams and ideas</h3>
-          <p>
-            All teams whom idea has been accepted into getlinked tech hackathon 1.0 2023 are formally announced
-          </p>
-        </VerticalTimelineElement>
-
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          contentStyle={{ background: '#D434FE', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid #D434FE' }}
-          date="November 18, 2023"
-          iconStyle={{ background: 'linear-gradient(270deg, #903AFF 0%, #FE34B9 100%)', color: '#fff' }}
-          icon={<TbNumber5 />}
-        >
-          <h3 className="vertical-timeline-e text-lg font-semibold">Getlinked Hackathon 1.0 Offically Begins</h3>
-          <p>
-            Accepted teams can now proceed to build their ground breaking skill driven solutions
-          </p>
-        </VerticalTimelineElement>
-
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          contentStyle={{ background: '#D434FE', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid #D434FE' }}
-          date="November 18, 2023"
-          iconStyle={{ background: 'linear-gradient(270deg, #903AFF 0%, #FE34B9 100%)', color: '#fff' }}
-          icon={<TbNumber6 />}
-        >
-          <h3 className="vertical-timeline-e text-lg font-semibold">Demo Day</h3>
-          <p>
-            Teams get the opportunity to pitch their projects to judges. The winner of the hackathon will also be announced on this day
-          </p>
-        </VerticalTimelineElement>
-
+    <div
+      className="flex flex-col items-center gap-4 lg:mb-[245px] pt-10 md:pt-20"
+      id="timeline"
+    >
+      <p className="font1 text-white text-3xl font-bold">Timeline</p>
+      <p className="font2 text-white text-sm mb-5 text-center px-7 lg:px-0">
+        Here is the breakdown of the time we anticipate using for the upcoming
+        event.
+      </p>
+      <VerticalTimeline lineColor="#D434FE">
+        {timelineData.map((data) => (
+          <VerticalTimelineElement
+            key={data.id}
+            date={data.time}
+            dateClassName=" text-[#D434FE] font2"
+            contentStyle={{
+              background: "transparent",
+              color: "#fff",
+              boxShadow: "none",
+            }}
+            contentArrowStyle={{
+              background: "transparent",
+              borderRight: "0px solid transparent",
+            }}
+            icon={data.id}
+            iconStyle={{
+              background: "linear-gradient(270deg, #903AFF 0%, #D434FE 100%)",
+              boxShadow: "none",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "24px",
+              fontWeight: "700",
+              color: "#fff",
+            }}
+            className="font2 border-0"
+          >
+            <h3 className="text-2xl mb-3 font-bold text-secondary-color">
+              {data.title}
+            </h3>
+            <p className="text-sm text-white">{data.desc}</p>
+          </VerticalTimelineElement>
+        ))}
       </VerticalTimeline>
-
-    </section>
-  )
+    </div>
+  );
 }
 
 export default TimelineComp
